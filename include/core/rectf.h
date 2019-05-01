6d44d88d8676b5e57c7d4f76414e2b0949b3ee2e
sombrerosheep <sombrerosheep@gmail.com>
2019-04-30 20:30:49 -0500

    Add rectf implementation
    like an SDL_Rect but with floats

---
+++ include/core/rectf.h
@@ -0,0 +1,21 @@
+#ifndef _FLOATY_CORE_RECTF
+#define _FLOATY_CORE_RECTF
+
+#include <SDL.h>
+#include <core/vec.h>
+
+typedef struct rectf {
+  float x;
+  float y;
+  float w;
+  float h;
+} rectf;
+
+inline SDL_bool vec2f_in_rectf(const vec2f *vec, const rectf *rect) {
+  return vec->x > rect->x &&
+         vec->x < rect->x + rect->w &&
+         vec->y > rect->y &&
+         vec->y < rect->y + rect->h;
+}
+
+#endif
