dfc772f4d8413f85d3159d3fc289821d0f332c5e
sombrerosheep <sombrerosheep@gmail.com>
2019-05-03 22:29:09 -0500

    copy incoming geometry into struct
    this way the struct owns it and can free it properly

--- include/World.h
+++ include/World.h
@@ -15,7 +15,7 @@ typedef struct World {
   void (*draw)(struct World *world, SDL_Renderer *renderer);
 } World;
 
-World* new_world(SDL_Rect *rects, int num_rects);
+World* new_world(const SDL_Rect *rects, int num_rects);
 void free_world(World *world);
 
 #endif // _FLOATY_WORLD
--- src/World.c
+++ src/World.c
@@ -13,10 +13,12 @@ void free_world(World *world) {
   SDL_free(world);
 }
 
-World* new_world(SDL_Rect *rects, int num_rects) {
+World* new_world(const SDL_Rect *rects, int num_rects) {
   World *w = (World*)SDL_calloc(1, sizeof(World));
-  w->geometry.rects = rects;
+
   w->geometry.count = num_rects;
+  w->geometry.rects = SDL_calloc(num_rects, sizeof(SDL_Rect));
+  SDL_memcpy(w->geometry.rects, rects, sizeof(SDL_Rect) * num_rects);
 
   w->draw = world_draw;
   w->update = world_update;
