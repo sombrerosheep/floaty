1ab4b70e928321eb9a6a9e85199819d1b92852c5
sombrerosheep <sombrerosheep@gmail.com>
2019-06-17 21:07:51 -0500

    add vec2f funcs for magnitude and normalized

--- include/core/vec2f.h
+++ include/core/vec2f.h
@@ -9,5 +9,8 @@ typedef struct vec2f {
 
 SDL_Point vec2f_to_sdl_point(const vec2f *vec);
 vec2f vec2f_add_vec2f(const vec2f *vec, const vec2f *delta);
+vec2f vec2f_mul_float(const vec2f *vec, float f);
+float vec2f_magnitude(const vec2f *vec);
+vec2f vec2f_normalized(const vec2f *vec);
 
 #endif
--- src/core/vec2f.c
+++ src/core/vec2f.c
@@ -17,3 +17,34 @@ vec2f vec2f_add_vec2f(const vec2f *vec, const vec2f *delta) {
 
   return new_vec;
 }
+
+vec2f vec2f_mul_float(const vec2f *vec, float f) {
+  vec2f result = {
+    vec->x * f,
+    vec->y * f
+  };
+
+  return result;
+}
+
+float vec2f_magnitude(const vec2f *vec) {
+  float sums = SDL_powf(vec->x, 2.f) + SDL_powf(vec->y, 2.f);
+  float result = SDL_sqrtf(sums);
+
+  return result;
+}
+
+vec2f vec2f_normalized(const vec2f *vec) {
+  vec2f result;
+  float mag = vec2f_magnitude(vec);
+
+  if (mag == 0.f) {
+    result.x = 0.f;
+    result.y = 0.f;
+  } else {
+    result.x = vec->x / mag;
+    result.y = vec->y / mag;
+  }
+
+  return result;
+}
